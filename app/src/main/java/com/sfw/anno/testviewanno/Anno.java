3f3d2ba83f05de91c68111aee5408dd09577582c
shufengwu <sfwandroid@163.com>
2016-11-25 01:28:10 +0800

    完善动态代理View事件注解

--- app/src/main/java/com/sfw/anno/testviewanno/Anno.java
+++ app/src/main/java/com/sfw/anno/testviewanno/Anno.java
@@ -4,29 +4,38 @@ import android.app.Activity;
 import android.view.View;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Shufeng.Wu on 2016/11/24.
  */
 
 public class Anno {
-    public static void inject(Activity activity){
-        Field [] fields;
+    public static void inject(Activity activity) {
+        Field[] fields;
         try {
+            //获取activity的所有变量
             fields = activity.getClass().getDeclaredFields();
-
-            for(Field field:fields){
+            //遍历所有变量
+            for (Field field : fields) {
                 field.setAccessible(true);
-                if(field.isAnnotationPresent(InjectView.class)){
-
+                //如果变量被@InjectView注解
+                if (field.isAnnotationPresent(InjectView.class)) {
+                    //获取InjectView对象
                     InjectView injectView = field.getAnnotation(InjectView.class);
-                    //利用反射调用findViewById方法
-                    Method mActivity_findViewById = activity.getClass().getMethod("findViewById",int.class);
-                    View view = (View) mActivity_findViewById.invoke(activity,injectView.value());
+                    //利用反射获得findViewById方法
+                    Method mActivity_findViewById = activity.getClass().getMethod("findViewById", int.class);
+                    //利用注解参数生成view对象
+                    View view = (View) mActivity_findViewById.invoke(activity, injectView.value());
+                    //不利用反射
                     //View view = activity.findViewById(injectView.value());
-                    field.set(activity,view);
+
+                    field.set(activity, view);
                 }
             }
         } catch (IllegalAccessException e) {
@@ -39,26 +48,38 @@ public class Anno {
 
     }
 
-    public static void onClickInject(final Activity activity){
-        Method [] methods;
-        methods = activity.getClass().getDeclaredMethods();
-        for (final Method method:methods){
+    /*
+    未使用动态代理
+     */
+    public static void onClickInject(final Activity activity) {
+        //获取所有方法
+        Method[] methods= activity.getClass().getDeclaredMethods();
+        //遍历所有方法
+        for (final Method method : methods) {
             method.setAccessible(true);
-            if(method.isAnnotationPresent(OnClick.class)){
+            //如果方法被@OnClick注解
+            if (method.isAnnotationPresent(OnClick.class)) {
+                //获得OnClick的对象
                 OnClick click = method.getAnnotation(OnClick.class);
-                int [] values = click.value();
-                for (int value:values){
+                //获得注解参数
+                int[] values = click.value();
+                for (int value : values) {
                     Method mActivity_findViewById = null;
                     Method mView_setOnClickListener = null;
                     try {
-                        mActivity_findViewById = activity.getClass().getMethod("findViewById",int.class);
-                        final View view = (View) mActivity_findViewById.invoke(activity,value);
+                        //利用反射获得findViewById方法
+                        mActivity_findViewById = activity.getClass().getMethod("findViewById", int.class);
+                        //获取view
+                        final View view = (View) mActivity_findViewById.invoke(activity, value);
+                        //利用反射获得setOnClickListener方法
                         mView_setOnClickListener = View.class.getMethod("setOnClickListener", View.OnClickListener.class);
+                        //利用反射调用setOnClickListener方法
                         mView_setOnClickListener.invoke(view, new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 try {
-                                    method.invoke(activity,v);
+                                    //反射调用method方法
+                                    method.invoke(activity, v);
                                 } catch (IllegalAccessException e) {
                                     e.printStackTrace();
                                 } catch (InvocationTargetException e) {
@@ -78,8 +99,77 @@ public class Anno {
         }
     }
 
-    public static void onClickProxyInject(Activity activity){
-        
+    /*
+    使用动态代理
+     */
+
+    public static void onClickProxyInject(Activity activity) {
+        //获取所有方法
+        Method[] methods = activity.getClass().getDeclaredMethods();
+        //遍历所有方法
+        for (final Method method : methods) {
+            method.setAccessible(true);
+            //如果方法被@OnClick注解
+            if (method.isAnnotationPresent(OnClick.class)) {
+                //获得OnClick的对象
+                OnClick click = method.getAnnotation(OnClick.class);
+                //获得注解参数
+                int[] values = click.value();
+                for (int value : values) {
+                    Method mActivity_findViewById = null;
+                    Method mView_setOnClickListener = null;
+                    try {
+                        //利用反射获得findViewById方法
+                        mActivity_findViewById = activity.getClass().getMethod("findViewById", int.class);
+                        View view = (View) mActivity_findViewById.invoke(activity, value);
+                        //利用反射获得setOnClickListener方法
+                        mView_setOnClickListener = View.class.getMethod("setOnClickListener", View.OnClickListener.class);
+                        mView_setOnClickListener.setAccessible(true);
+                        //
+                        InjectInvocationHandler injectInvocationHandler = new InjectInvocationHandler(activity);
+                        injectInvocationHandler.add("onClick", method);
+                        //View.OnClickListener代理类
+                        Object o = Proxy.newProxyInstance(View.OnClickListener.class.getClassLoader(), new Class[]{View.OnClickListener.class}, injectInvocationHandler);
+                        //反射执行setOnClickListener方法
+                        mView_setOnClickListener.invoke(view, o);
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static class InjectInvocationHandler implements InvocationHandler {
+
+        private Object object;
+        private Map<String, Method> map = new HashMap<>();
+
+        public InjectInvocationHandler(Object object) {
+            this.object = object;
+        }
+
+        //当代理类调用真实类的方法时调用
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            //获得代理类执行的方法名
+            String name = method.getName();
+            //获得被注解的方法
+            Method m = map.get(name);
+            if (m != null)
+                //执行此方法
+                m.invoke(object, args);
+            return null;
+        }
+
+        public void add(String name, Method method) {
+            map.put(name, method);
+        }
+
     }
 }
 
--- app/src/main/java/com/sfw/anno/testviewanno/MainActivity.java
+++ app/src/main/java/com/sfw/anno/testviewanno/MainActivity.java
@@ -1,8 +1,7 @@
 package com.sfw.anno.testviewanno;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Anno.inject(this);
-        Anno.onClickInject(this);
+        //Anno.onClickInject(this);
+        Anno.onClickProxyInject(this);
     }
 
     @OnClick({R.id.show_tv1,R.id.show_tv2})
